7a6b20da5e93fe25a100a46a1fe65de42e5a2f22
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2021-10-07 20:39:16 +0300

    remove unnecessary

--- src/extension.ts
+++ src/extension.ts
@@ -4,29 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { window, commands, ExtensionContext } from 'vscode';
-import { showQuickPick, showInputBox } from './basicInput';
 import { multiStepInput } from './multiStepInput';
-import { quickOpen } from './quickOpen';
 
 export function activate(context: ExtensionContext) {
 	context.subscriptions.push(commands.registerCommand('samples.quickInput', async () => {
-		// const options: { [key: string]: (context: ExtensionContext) => Promise<void> } = {
-		// 	showQuickPick,
-		// 	showInputBox,
-		// 	multiStepInput,
-		// 	quickOpen,
-		// };
-		// console.log(options);
-		// const quickPick = window.createQuickPick();
-		// quickPick.items = Object.keys(options).map(label => ({ label }));
-		// quickPick.onDidChangeSelection(selection => {
-		// 	if (selection[0]) {
-		// 		options[selection[0].label](context)
-		// 			.catch(console.error);
-		// 	}
-		// });
-		// quickPick.onDidHide(() => quickPick.dispose());
-		// quickPick.show();
 		multiStepInput(context).catch(console.error);
 	}));
 }
